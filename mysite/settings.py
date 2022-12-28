baccc9c90b032d7aa69c9288fc3cb25f3848443c
Uttam patel <utampipaliya@gmail.com>
2022-12-28 19:21:50 +0530

    up

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^2wfd95xj0zdrfkgu$!aq)3u!@2vr==e21^(3&^$pp51wx73ay
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["uttampipaliya.me",""]
+ALLOWED_HOSTS = []
 
 
 # Application definition
